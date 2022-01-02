8a12e35b1011c1bc9ae89e4721f996ee37899c2e
Hickar <hickar@icloud.com>
2022-01-02 15:32:33 +0300

    docker-compose configuration added

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"log"
 
-	"gopkg.in/tucnak/telebot.v3"
 	"sound-seeker-bot/internal/bot"
 	"sound-seeker-bot/internal/config"
 )
@@ -16,14 +15,10 @@ var (
 func main() {
 	flag.Parse()
 
-	if *configPath == "" {
-		log.Fatalln("no \"configuration filepath was provided\"")
-	}
-
 	conf, err := config.NewConfig(*configPath)
 	if err != nil {
 		log.Fatalf("unable to parse configuration file at %s: %s", *configPath, err.Error())
 	}
 
-	bot.NewBot(conf)
+	bot.NewBot(*conf)
 }
